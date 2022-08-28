d8d1bb89f2ede49c212ac5191b36d1adad8dd358
Diego Cantero <diego.cantero@n5now.com>
2022-08-28 13:05:20 -0500

    ♻️ Order code, using vars, remove code, config env fil config

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import {Characters } from '../models/character';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
 
-  search(query: string): Observable<Characters> {
-    return this.http.get<Characters>(`https://rickandmortyapi.com/api/character?name=${query}`);
-  }
-
   constructor(private http: HttpClient) { }
 
+  search(query: string): Observable<Characters> {
+    return this.http.get<Characters>(`${environment.API_URL}/character?name=${query}`);
+  }
+
   getCharacters(): Observable<Characters> {
-    return this.http.get<Characters>('https://rickandmortyapi.com/api/character');
+    return this.http.get<Characters>(`${environment.API_URL}/character`);
   }
 }
